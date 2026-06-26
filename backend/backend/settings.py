96bf2cba626de54dab9a3dcaadd2c5591e72f751
roi-zango <foundikounjikam547@gmail.com>
2026-06-26 23:41:49 +0100

    Fix CORS configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = "https://innovatech-swart.vercel.app"
+CORS_ALLOWED_ORIGINS = "https://innovatech-swart.vercel.app"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
